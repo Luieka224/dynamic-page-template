5b1bcd92c563635522a7a963064485039ef86fde
Jed Adrian Denosta <59909864+JedAdrianDenosta@users.noreply.github.com>
2022-05-19 17:27:04 +0800

    add index.js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,21 @@
-import Landing from '/components/landing.jsx'
-import Dashboard from '/components/dashboard.jsx'
+import Landing from "/components/landing.jsx";
+import Dashboard from "/components/dashboard.jsx";
 
-import { useState } from 'react'
+import { useState } from "react";
 
-function Home() {
-  const [isSignedIn, setSignedIn] = useState(false)
+export default function Home() {
+  const [isSignedIn, setSignedIn] = useState(false);
 
   function signInAndOut() {
-    setSignedIn(!isSignedIn)
+    setSignedIn(!isSignedIn);
   }
 
   return (
     <main>
-        {
-          isSignedIn
-          ? <Dashboard content="Currently in Dashboard" />
-          : <Landing content = "Currently in Landing "/>
-        }
-        <button onClick={signInAndOut}>
-          {
-            isSignedIn
-            ? "Sign Out"
-            : "Sign In"
-          }
-        </button>
+      {isSignedIn ? <Dashboard></Dashboard> : <Landing></Landing>}
+      <button onClick={signInAndOut}>
+        {isSignedIn ? "Sign Out" : "Sign In"}
+      </button>
     </main>
-  )
+  );
 }
-
-export default Home;
